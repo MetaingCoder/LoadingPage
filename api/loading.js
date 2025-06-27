84dd62d1a779dcf814e9a8be68af26a4d4e7490d
MetaingCoder <Meta-online@outlook.com>
2025-06-27 22:02:09 +0800

    Create loading.js

---
+++ api/loading.js
@@ -0,0 +1,21 @@
+fetch('https://loading.earth-online.space/res/loading.html')
+    .then(response => response.text())
+    .then(html => {
+        const loader = document.createElement('div');
+        loader.innerHTML = html;
+        loader.style.cssText = `
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            z-index: 9999;
+        `;
+        document.body.appendChild(loader);
+
+        // 直接使用 setTimeout，不依赖 load 事件
+        setTimeout(() => {
+            loader.remove();
+        }, 2000);
+    })
+    .catch(err => console.error('加载动画失败:', err));
